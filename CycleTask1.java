e3ac21890836d9169a91e3e096ad25678d6c3f4c
Anton <75360788+AntonNovosad@users.noreply.github.com>
2020-12-07 21:51:32 +0300

    Add files via upload

---
+++ CycleTask1.java
@@ -0,0 +1,19 @@
+/*
+Напишите программу, где пользователь вводит любое целое положительное число.
+А программа суммирует все числа от 1 до введенного пользователем числа.
+ */
+
+import java.util.Scanner;
+
+public class CycleTask1 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.println("Введите любое положительное число: ");
+        int a = num.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= a; i++){
+            sum += i;
+        }
+        System.out.println("Сумма всех чисел = " + sum);
+    }
+}
---
+++ CycleTask4.java
@@ -0,0 +1,15 @@
+/*
+Составить программу нахождения произведения квадратов первых двухсот чисел.
+ */
+
+public class CycleTask4 {
+    public static void main(String[] args) {
+        double Pr = 1.0;
+        for (int i = 1; i <= 200; i ++) {
+            Pr *= Math.pow(i, 2);
+        }
+        System.out.println("Произведения квадратов первых двухсот чисел = " + Pr);
+        // получается число намного больше размера "long"
+    }
+
+}
