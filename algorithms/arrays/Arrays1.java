f2160ab62c296f0b55b4b8f726a6c78db69c81e7
Anton Novosad <noobito.2011@gmail.com>
2020-12-15 17:12:58 +0300

    Add answer of task Arrays1

--- algorithms/arrays/Arrays1.java
+++ algorithms/arrays/Arrays1.java
@@ -1,4 +1,20 @@
+/*
+В массив А[N] занесены натуральные числа.
+Найти сумму тех элементов, которые кратны данному К.
+ */
+
 package algorithms.arrays;
 
 public class Arrays1 {
+    public static void main(String[] args) {
+        int K = 2;
+        int sum = 0;
+        int[] array = new int[]{4, 6, 7, 8};
+        for (int j : array) {
+            if (j % K == 0) {
+                sum += j;
+            }
+        }
+        System.out.println("The sum of the elements = " + sum);
+    }
 }
