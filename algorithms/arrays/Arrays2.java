fb56f4cd6eac3fe8431fbfbe3f9686dfdc6c0de1
Anton Novosad <noobito.2011@gmail.com>
2020-12-16 18:36:08 +0300

    Add arrays2

---
+++ algorithms/arrays/Arrays2.java
@@ -0,0 +1,23 @@
+/*
+Дана последовательность действительных чисел а1, а2, а3, ..., аn.
+Заменить все ее члены, больше данного Z, этим числом.
+Подсчитать кол-во замен.
+ */
+
+package algorithms.arrays;
+
+import java.util.Arrays;
+
+public class Arrays2 {
+    public static void main(String[] args) {
+            int Z = 3;
+            int[] array = new int[]{1, 2, 3, 4, 5, 6};
+            for (int i = 0; i < array.length; i++){
+                if(array[i] > Z){
+                    array[i] = Z;
+                }
+            }
+            System.out.println(Arrays.toString(array));
+            System.out.println("Number of changes = "  );
+        }
+    }
