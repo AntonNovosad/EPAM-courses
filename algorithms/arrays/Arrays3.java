763053a08f1a3e0ba7ef832f4e2654e555cb946b
Anton Novosad <noobito.2011@gmail.com>
2020-12-16 18:47:40 +0300

    Add Arrays3

--- algorithms/arrays/Arrays3.java
+++ algorithms/arrays/Arrays3.java
@@ -1,11 +1,27 @@
 /*
-
+Дан массив действительных чисел, размерность которого N.
+Подсчитать, сколько в нем отрицательных, положительных и нулевых элементов.
  */
 
 package algorithms.arrays;
 
 public class Arrays3 {
     public static void main(String[] args) {
-
+        double[] array = {-2.0, 4.0, 6.0, -1.0, 0.0};
+        int minus = 0;
+        int plus = 0;
+        int zero = 0;
+        for (double v : array) {
+            if (v > 0) {
+                plus++;
+            } else if (v < 0) {
+                minus++;
+            } else {
+                zero++;
+            }
+        }
+     System.out.println("Negative values: " + minus);
+     System.out.println("Positive values: " + plus);
+     System.out.println("Zero values: " + zero);
     }
 }
