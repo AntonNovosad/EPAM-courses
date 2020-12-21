e1ef36ecbe4c8f10336bcfbc6fa2d9deb8cb3899
Anton Novosad <noobito.2011@gmail.com>
2020-12-21 21:22:56 +0300

    add Arrays6

---
+++ algorithms/arrays/Arrays6.java
@@ -0,0 +1,30 @@
+/*
+Задана последовательность N вещественных чисел.
+Вычислить сумму чисел, порядковые номера которых являются простыми числами.
+ */
+
+package algorithms.arrays;
+
+public class Arrays6 {
+    public static void main(String[] args) {
+        int sum = 0;
+        double[] array = {1.0, 2.0, 5.0, 4.0, 8.0, 11.0};
+        boolean b = true;
+        for (int i = 2; i <= array.length; i++) {
+            for (int P = 2; P < i; P++) {
+                if (i % P == 0) {
+                    b = false;
+                    break;
+                }
+            }
+            if (b) {
+                System.out.println(i); //Вывод простых чисел
+                sum += array[i];
+            } // Сумма значений простых числе i
+            else b = true;
+        }
+        System.out.println(sum);
+    }
+}
+
+
