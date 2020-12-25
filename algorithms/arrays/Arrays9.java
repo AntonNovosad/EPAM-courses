5997c697a824a19bdec152c9392708bfdfb3ebd9
Anton Novosad <noobito.2011@gmail.com>
2020-12-25 19:30:16 +0300

    fix Arrays9

--- algorithms/arrays/Arrays9.java
+++ algorithms/arrays/Arrays9.java
@@ -4,18 +4,16 @@
  */
 
 package algorithms.arrays;
-import java.util.Arrays;
-
 
 public class Arrays9 {
     public static void main(String[] args) {
-        boolean b =true;
-        int[] array = {1,2,5,3,4,7,2,3,9};
-            for(int i = 0; i < array.length; i++){
-                for(int j = i + 1; j < array.length; j++)
-                    if (array[i] == array[j]){    // сравнение значений в массиве
-                        System.out.println("Одинаковое число: " + array[i]);
-                    }
-            }
+        boolean b = true;
+        int[] array = {1, 2, 5, 3, 4, 7, 2, 3, 9};
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++)
+                if (array[i] == array[j]) {    // сравнение значений в массиве
+                    System.out.println("Одинаковое число: " + array[i]);
+                }
+        }
     }
 }
