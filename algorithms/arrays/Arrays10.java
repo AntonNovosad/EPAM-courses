fae314d684e6feff39af190d6fe7f48c17d0f119
Anton Novosad <noobito.2011@gmail.com>
2020-12-25 19:45:32 +0300

    fix Arrays10

--- algorithms/arrays/Arrays10.java
+++ algorithms/arrays/Arrays10.java
@@ -11,10 +11,10 @@ import java.util.Arrays;
 public class Arrays10 {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        for (int i = 0; i < array.length; i++) {
-            if (i % 2 != 0)
+        for (int i = 0; i < array.length; i++)
+            if (i % 2 != 0) {
                 array[i] = 0;
-        }
+            }                    //не знаю как дальше!
         System.out.println(Arrays.toString(array));
     }
 }
