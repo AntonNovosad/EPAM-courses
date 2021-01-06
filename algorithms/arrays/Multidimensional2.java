ff97c7b65ff464a31052fd1705630950a3e3dab4
Anton Novosad <noobito.2011@gmail.com>
2021-01-06 20:55:16 +0300

    fix multi2

--- algorithms/arrays/Multidimensional2.java
+++ algorithms/arrays/Multidimensional2.java
@@ -10,20 +10,18 @@ import java.util.Scanner;
 public class Multidimensional2 {
     public static void main(String[] args) {
         int[][] array;
-        int n = 4; //высота матрицы
-        int m = 4; //ширина матрицы
         Scanner num = new Scanner(System.in);
-        array = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            System.out.println((i + 1) + "-я строка матрицы: ");
-            for (int j = 0; j < m; j++) {
+        array = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            System.out.print((i + 1) + "-я строка матрицы: ");
+            for (int j = 0; j < 4; j++) {
                 array[i][j] = num.nextInt();
             }
         }
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 4; j++){
                 if (j == i){
-                    System.out.println(array[i][j]);
+                    System.out.print(array[i][j]);
                 }
             }
         }
