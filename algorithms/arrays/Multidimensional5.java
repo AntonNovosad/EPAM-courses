7607be0227d12fbc8fc65c72a644d8d455fe132d
Anton Novosad <noobito.2011@gmail.com>
2021-01-06 21:58:49 +0300

    add multi5

---
+++ algorithms/arrays/Multidimensional5.java
@@ -0,0 +1,34 @@
+/*
+Сформировать квадратную матрицу порядка N по заданному образцу(N - четное)
+( 1, 1, 1,..., 1, 1, 1)
+( 2, 2, 2,..., 2, 2, 0)
+( 3, 3, 3,..., 3, 0, 0)
+...
+(N-1,N-1,0,..., 0, 0, 0)
+(N, 0, 0,..., 0, 0, 0)
+ */
+
+package algorithms.arrays;
+
+import java.util.Scanner;
+
+public class Multidimensional5 {
+    public static void main(String[] args) {
+        int n;
+        int[][] array;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите размер матрицы: ");
+        n = scanner.nextInt();
+        array = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            System.out.println();
+            for (int j = 0; j < n; j++) {
+                array[i][j] = i + 1;
+                if (j >= n - i) {
+                    array[i][j] = 0;
+                }
+                System.out.print(array[i][j] + " ");
+            }
+        }
+    }
+}
