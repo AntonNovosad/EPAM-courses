02c6874b15732465db4cba1f26ec89b7c792b6bb
Anton Novosad <noobito.2011@gmail.com>
2021-01-08 22:15:14 +0300

    fix multi5

--- algorithms/arrays/Multidimensional5.java
+++ algorithms/arrays/Multidimensional5.java
@@ -15,11 +15,14 @@ import java.util.Scanner;
 public class Multidimensional5 {
     public static void main(String[] args) {
         int n;
-        int[][] array;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите размер матрицы: ");
         n = scanner.nextInt();
-        array = new int[n][n];
+        solutionMultidimensional5(n);
+    }
+
+    private static void solutionMultidimensional5(int n) {
+        int[][] array = new int[n][n];
         for (int i = 0; i < n; i++) {
             System.out.println();
             for (int j = 0; j < n; j++) {
