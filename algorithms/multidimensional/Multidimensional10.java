75590f819ec6ea6035028ec673728c08c617d67f
Anton Novosad <noobito.2011@gmail.com>
2021-01-11 17:57:34 +0300

    add solution Multidimensional10.java

--- algorithms/multidimensional/Multidimensional10.java
+++ algorithms/multidimensional/Multidimensional10.java
@@ -1,7 +1,38 @@
+/*
+Найти положительные элементы главной диагонали квадратной матрицы.
+ */
+
 package algorithms.multidimensional;
 
 public class Multidimensional10 {
     public static void main(String[] args) {
+        int sizeOfMatrix = MultidimensionalUtils.inputSquareMatrixSize();
+        int[][] squareMatrix = new int[sizeOfMatrix][sizeOfMatrix];
+        int max = 10;
+        int min = -10;
+        for (int i = 0; i < sizeOfMatrix; i++) {
+            for (int j = 0; j < sizeOfMatrix; j++) {
+                squareMatrix[i][j] = randomInt(max, min);
+            }
+        }
+        System.out.println("Изначальная матрица: ");
+        MultidimensionalUtils.printIntMatrix(squareMatrix);
 
+        positiveDiagonal(squareMatrix);
+
+    }
+
+    private static void positiveDiagonal(int[][] matrix) {
+        System.out.println("Положительные элементы главной диагонали матрицы: ");
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][i] > 0) {
+                System.out.print(matrix[i][i] + " ");
+            }
+        }
+    }
+
+    private static int randomInt(int max, int min) {
+        double random = Math.random() * (max - min) + min;
+        return (int) Math.round(random);
     }
 }
