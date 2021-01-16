6c782b47ed787bfc827d3cc010537df15d0237f4
Anton Novosad <noobito.2011@gmail.com>
2021-01-16 12:43:17 +0300

    change solution - add function getMostPopularValue and randomInt

--- algorithms/arrays/Arrays9.java
+++ algorithms/arrays/Arrays9.java
@@ -7,13 +7,47 @@ package algorithms.arrays;
 
 public class Arrays9 {
     public static void main(String[] args) {
-        boolean b = true;
-        int[] array = {1, 2, 5, 3, 4, 7, 2, 3, 9};
+        int maxValue = 10;
+        int minValue = 0;
+        int[] array = new int[10];
+        System.out.println("Исходная матрица: ");
         for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++)
-                if (array[i] == array[j]) {    // сравнение значений в массиве
-                    System.out.println("Одинаковое число: " + array[i]);
-                }
+            array[i] = randomInt(maxValue, minValue);
+            System.out.print(array[i] + " ");
         }
+        System.out.println();
+
+        System.out.println("Ответ по условию: ");
+        MostPopularValue(array);
+    }
+
+    public static void MostPopularValue(int[] array) {
+        int[] popularValue = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    popularValue[i]++;
+                }
+            }
+        }
+
+        int valueResult = array[0];
+        int popularValueResult = popularValue[0];
+        for (int i = 0; i < popularValue.length; i++) {
+            if (popularValue[i] > popularValueResult) {
+                popularValueResult = popularValue[i];
+                valueResult = array[i];
+            }
+            if ((popularValueResult == popularValue[i]) && (array[i] < valueResult)) {
+                valueResult = array[i];
+            }
+        }
+        System.out.println(valueResult);
+    }
+
+    private static int randomInt(int max, int min) {
+        double random = Math.random() * (max - min) + min;
+        return (int) Math.round(random);
     }
 }
